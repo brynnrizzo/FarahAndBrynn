b2b21a19a7ddc1d45db12e77e7f0942004306d27
farahabdi13 <farah.abdi@mykenzie.snhu.edu>
2023-09-23 13:21:04 -0700

    merge commit

---
+++ Application/src/main/java/com/kenzie/appserver/controller/JobListingController.java
@@ -0,0 +1,65 @@
+package com.kenzie.appserver.controller;
+
+import com.kenzie.appserver.controller.model.JobListingCreateRequest;
+import com.kenzie.appserver.controller.model.JobListingResponse;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+//Eva's task #2 out of 3 for Sprint 1
+@RestController
+@RequestMapping("/job")
+public class JobListingController {
+    /*
+    once it merges I can uncomment this b/c I need the job listing service
+
+    private JobListingService jobListingService;
+    JobController(JobListingService jobListingService) {this.jobListingService = jobListingService;}
+     */
+
+    //TODO 1.Handles job search by keyword
+    //User Story: As a job seeker I want to search for jobs when I type a keyword(s)
+    //It should have endpoints to perform keyword-based searches and return a list of matching job listings.
+    @GetMapping("/search") //edit annotation if needed
+    public ResponseEntity<JobListingResponse> searchJobsByKeyword(@RequestParam("keywords") List<String> keywords) {
+
+        //more code here
+
+        JobListingResponse jobListingResponse = new JobListingResponse(); //edit this if needed
+
+        //more code here
+
+        return ResponseEntity.ok(jobListingResponse); //edit this if needed
+
+    }
+
+    //TODO 2.Handles filtering job listings based on criteria - still thinking this one
+    @GetMapping("/filter") //edit annotation if needed
+    public ResponseEntity<List<JobListingResponse>> filterJobListings(@RequestParam("criteria") String criteria){
+
+        //code here
+
+        return null; //edit this when ready
+    }
+
+    //TODO 3.Handles posting new job listings
+    @PostMapping("/create") //edit annotation if needed
+    public ResponseEntity<JobListingResponse> createJobListing(@RequestBody JobListingCreateRequest request){
+
+        //code here
+
+        return null; //edit this when ready
+    }
+
+    //TODO 4.Handles updating the job listing
+    @PutMapping("/update/{jobID}") //edit annotation if needed
+    public ResponseEntity<JobListingResponse> updateJobListing(@PathVariable("jobID") String jobID, @RequestBody JobListingCreateRequest request){
+
+        //code here
+
+        return null; //edit this when ready
+    }
+
+}
---
+++ Application/src/main/java/com/kenzie/appserver/controller/model/JobListingResponse.java
@@ -0,0 +1,118 @@
+package com.kenzie.appserver.controller.model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
+//Eva's task #1 out of 3 for Sprint 1
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class JobListingResponse {
+    @JsonProperty("jobID")
+    private String jobID;
+
+    @JsonProperty("jobTitle")
+    private String jobTitle;
+
+    @JsonProperty("jobDescription")
+    private String jobDescription;
+
+    @JsonProperty("location")
+    private String location;
+
+    @JsonProperty("status")
+    private boolean status;
+
+    @JsonProperty("Salary")
+    private double salary;
+
+    @JsonProperty("Skills")
+    private List<String> skills;
+
+    @JsonProperty("jobType")
+    private String jobType;
+
+    @JsonProperty("employerID")
+    private String employerID;
+
+    @JsonProperty("keywords")
+    private List<String> keywords;
+
+    public String getJobID() {
+        return jobID;
+    }
+
+    public void setJobID(String jobID) {
+        this.jobID = jobID;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public String getJobDescription() {
+        return jobDescription;
+    }
+
+    public void setJobDescription(String jobDescription) {
+        this.jobDescription = jobDescription;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public List<String> getSkills() {
+        return skills;
+    }
+
+    public void setSkills(List<String> skills) {
+        this.skills = skills;
+    }
+
+    public String getJobType() {
+        return jobType;
+    }
+
+    public void setJobType(String jobType) {
+        this.jobType = jobType;
+    }
+
+    public String getEmployerID() {
+        return employerID;
+    }
+
+    public void setEmployerID(String employerID) {
+        this.employerID = employerID;
+    }
+
+    public List<String> getKeywords() {return keywords;}
+
+    public void setKeywords(List<String> keywords) {this.keywords = keywords;}
+
+    //add equals & hashcode and toString later if needed
+}
