b76ca26dbff4652f1f293c60a8c73209b97c0fdd
larae23 <eva.lara@mykenzie.snhu.edu>
2023-09-22 19:58:53 -0700

    added the JobController class to the controller package

---
+++ Application/src/main/java/com/kenzie/appserver/controller/JobController.java
@@ -0,0 +1,26 @@
+package com.kenzie.appserver.controller;
+
+import com.kenzie.appserver.service.ExampleService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//Eva's task #2 out of 3 for Sprint 1
+@RestController
+@RequestMapping("/job")
+public class JobController {
+    /*
+    once it merges I can uncomment this b/c I need the job listing service
+
+    private JobListingService jobListingService;
+    JobController(JobListingService jobListingService) {this.jobListingService = jobListingService;}
+     */
+
+    //TODO Handles job search by keyword
+
+    //TODO Handles filtering job listings based on criteria
+
+    //TODO Handles posting new job listings
+
+    //TODO Handles updating the job listing
+    
+}
